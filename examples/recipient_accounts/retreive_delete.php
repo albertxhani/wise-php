0e0c4342a1942eabb6c90c5a55a227367fa6e834
Albert Xhani <albert.xhani@outlook.com>
2022-01-18 10:29:06 +0100

    Examples - recepient_account retrieve and delete

---
+++ examples/recipient_accounts/retreive_delete.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once  __DIR__ . "/../init.php";
+
+$test_id = 223537602;
+
+try {
+
+    /**
+     * Retrieve account
+     */
+    $account = $client->recipient_accounts->retrieve($test_id);
+
+    echo "<ore>";
+    print_r($account);
+
+    /**
+     * Delete account
+     */
+    $response = $client->recipient_accounts->delete($test_id);
+
+    print_r($response);
+
+} catch (\TransferWise\Exception\AccessException $e) {
+    echo "account must be deleted already";
+}
+
+
